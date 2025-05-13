ae8fc2ff54b3600fb7f7af83e73c9e066f975e03
rajusw804 <46264561+rajusw804@users.noreply.github.com>
2025-05-13 16:38:07 +0530

    Update reconfig-replicaset.sh

--- reconfig-replicaset.sh
+++ reconfig-replicaset.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-
 # Connect to MongoDB and obtain the current replica set configuration
-current_cfg=$(mongosh --host 192.168.61.29 --quiet --eval 'rs.conf()')
-
+current_cfg=$(mongosh --host 192.168.61.29 --quiet --eval "rs.conf()")
 # Prepare the updated configuration with priority settings
-updated_cfg=$(echo "$current_cfg" | sed 's/\("host": "192.168.61.29:27017",$"priority": [0-9]/\1"priority": 2/' | \
-                                 sed 's/\("host": "192.168.61.66:27016",$"priority": [0-9]/\1"priority": 1/')
-
+updated_cfg=$(echo "$current_cfg" | sed 's/\"host\": \"192.168.61.29:27017\", \"priority\": [0-9]\.\?[0-9]*\/\1\"priority\": 2/' | sed 's/\"host\": \"192.168.61.66:27016\", \"priority\": [0-9]\.\?[0-9]*\/\1\"priority\": 1/')
 # Apply the updated replica set configuration
 mongosh --host 192.168.61.29 --quiet --eval "rs.reconfig($updated_cfg)"
-
 # Output the status of the replica set to verify changes
-mongosh --host 192.168.61.29 --quiet --eval 'rs.status()'
+mongosh --host 192.168.61.29 --quiet --eval "rs.status()"
